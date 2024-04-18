c1590033bc14e3ee67a588f2cdb5a1948079883f
KPCOFGS <a13375572211@gmail.com>
2024-04-17 20:40:02 -0400

    updated the map

---
+++ cdn/2/widgets/FeaturePanel/dist/runtime/translations/default.ts
@@ -0,0 +1,7 @@
+/**
+
+*/
+export default {
+  _widgetLabel: 'Feature Panel',
+  selectfeatures: "Select feature(s) in the map"
+}
---
+++ cdn/2/widgets/chunks/c72ee9ef783b90086ae9c4156175b2ed.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkexb_client"] = self["webpackChunkexb_client"] || []).push([["your-extensions_widgets_share-code-widgets_share-code-chunk_common_my-module_ts"],{
+
+/***/ "./your-extensions/widgets/share-code-widgets/share-code-chunk/common/fn.ts":
+/*!**********************************************************************************!*\
+  !*** ./your-extensions/widgets/share-code-widgets/share-code-chunk/common/fn.ts ***!
+  \**********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   f: () => (/* binding */ f)
+/* harmony export */ });
+function f() {
+    return 'In f.';
+}
+
+
+/***/ }),
+
+/***/ "./your-extensions/widgets/share-code-widgets/share-code-chunk/common/my-module.ts":
+/*!*****************************************************************************************!*\
+  !*** ./your-extensions/widgets/share-code-widgets/share-code-chunk/common/my-module.ts ***!
+  \*****************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   f1: () => (/* binding */ f1)
+/* harmony export */ });
+/* harmony import */ var _fn__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./fn */ "./your-extensions/widgets/share-code-widgets/share-code-chunk/common/fn.ts");
+
+function f1() {
+    return `In f1. ${(0,_fn__WEBPACK_IMPORTED_MODULE_0__.f)()}`;
+}
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2lkZ2V0cy9jaHVua3MvYzcyZWU5ZWY3ODNiOTAwODZhZTljNDE1NjE3NWIyZWQuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFPLFNBQVMsQ0FBQztJQUNmLE9BQU8sT0FBTztBQUNoQixDQUFDOzs7Ozs7Ozs7Ozs7Ozs7O0FDRnVCO0FBRWpCLFNBQVMsRUFBRTtJQUNoQixPQUFPLFVBQVUsc0NBQUMsRUFBRSxFQUFFO0FBQ3hCLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9leGItY2xpZW50Ly4veW91ci1leHRlbnNpb25zL3dpZGdldHMvc2hhcmUtY29kZS13aWRnZXRzL3NoYXJlLWNvZGUtY2h1bmsvY29tbW9uL2ZuLnRzIiwid2VicGFjazovL2V4Yi1jbGllbnQvLi95b3VyLWV4dGVuc2lvbnMvd2lkZ2V0cy9zaGFyZS1jb2RlLXdpZGdldHMvc2hhcmUtY29kZS1jaHVuay9jb21tb24vbXktbW9kdWxlLnRzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBmICgpIHtcbiAgcmV0dXJuICdJbiBmLidcbn0iLCJpbXBvcnQgeyBmIH0gZnJvbSAnLi9mbidcblxuZXhwb3J0IGZ1bmN0aW9uIGYxICgpIHtcbiAgcmV0dXJuIGBJbiBmMS4gJHtmKCl9YFxufVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
