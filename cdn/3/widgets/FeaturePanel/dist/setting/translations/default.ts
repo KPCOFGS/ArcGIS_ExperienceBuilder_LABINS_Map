be06a11a795ca864db07b25002b061f0972590e9
KPCOFGS <a13375572211@gmail.com>
2024-04-17 21:17:57 -0400

    updated the map

---
+++ cdn/3/widgets/FeaturePanel/dist/setting/translations/default.ts
@@ -0,0 +1,7 @@
+export default {
+  selectMapWidget: 'Select a map',
+  sourceLabel: 'Source',
+  autoOpenClose: 'Auto open and close widget parent if using an application template (like JewleryBox or Pocket) that has a Sidebar',
+  advice: 'Unless you want the map\'s pop-up to show the same data that this widget will show in its panel, you need to disable the map\'s pop-up by clicking on the map widget and in its settings dialog scroll down to the bottom in the Options section and turn on the "Disable pop-up" switch.',
+  widgetUseAdvice: 'Application Settings Advice'
+}
