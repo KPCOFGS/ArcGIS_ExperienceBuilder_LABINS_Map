1e78cff3f0e369b584adc0d4f85b3647c6f02101
KPCOFGS <a13375572211@gmail.com>
2024-06-11 16:35:55 -0400

    Updated

--- your-extensions/get-map-coordinates-class/src/runtime/translations/default.ts
+++ your-extensions/get-map-coordinates-class/src/runtime/translations/default.ts
@@ -1,6 +1,6 @@
 export default {
   _widgetLabel: 'Get Map Coordinates (Class)',
-  latLon: 'Lattt/Lon',
+  latLon: 'Lat/Lon',
   zoom: 'Zoom',
   latLonWillBeHere: 'Lat/Lon (None - please mouse over map)'
     ,gLink: 'https://labins.org'
